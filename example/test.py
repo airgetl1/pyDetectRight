c767ba1cd3bb25be67c127d80055c81ad9c957f2
Jamin <chenxm35@gmail.com>
2013-05-23 18:40:06 +0800

    Update test.py

--- example/test.py
+++ example/test.py
@@ -12,3 +12,7 @@ db = "/media/tera/workspace/eclipse/uadetector/db/detectright.data"
 handler = DetectRight(db)
 devmap = handler.getProfileFromUA("Android 4.1")
 print devmap
+
+
+uaprofile = "http://nds1.nds.nokia.com/uaprof/N6303iclassicr100.xml"
+print handler.getProfileFromUAProfile(uaprofile)
