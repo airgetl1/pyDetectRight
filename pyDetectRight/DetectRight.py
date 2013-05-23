e2e5795a15d6ed0acfceec93967560f931acc377
Jamin <chenxm35@gmail.com>
2013-05-23 18:39:13 +0800

    add some useful ports

--- pyDetectRight/DetectRight.py
+++ pyDetectRight/DetectRight.py
@@ -13,14 +13,45 @@ class DetectRight(object):
 		self.gateway.entry_point.initializeDetectRight(dbstring)
 
 
-	def getAllDevices(self):
-		return self.entry.getAllDevices()
+	def getAllDevices(self, ids = None):
+		if ids == None:
+			return self.entry.getAllDevices()
+		else:
+			java_list = ListConverter().convert(ids, self.gateway._gateway_client)
+			return self.entry.getAllDevices(java_list)
 
 
-	def getAllDevicesByIDs(self, ids):
-		java_list = ListConverter().convert(ids, self.gateway._gateway_client)
-		return self.entry.getAllDevices(java_list)
+	def getProfile(self, schema = None):
+		if schema is None:
+			return self.entry.getProfile()
+		else:
+			return self.entry.getProfile(schema)
+
+
+	def getProfileFromDevice(self, entitytype, category, description):
+		return self.entry.getProfileFromDevice(entitytype, category, description)
+
+
+	def getProfileFromDeviceID(self, id):
+		return self.entry.getProfileFromDeviceID(id)
+
+
+	def getProfileFromHeaders(self, lhm):
+		java_map = MapConverter().convert(lhm, self.gateway._gateway_client)
+		return self.entry.getProfileFromHeaders(java_map)
+
+
+	def getProfileFromPhoneID(self, id):
+		return self.entry.getProfileFromPhoneID(str(id))
+
+
+	def getProfileFromTAC(self, tac):
+		return self.entry.getProfileFromTAC(tac)
 
 
 	def getProfileFromUA(self, ua):
 		return self.entry.getProfileFromUA(ua)
+
+
+	def getProfileFromUAProfile(self, uap):
+		return self.entry.getProfileFromUAProfile(uap)
